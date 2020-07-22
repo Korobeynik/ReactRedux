c575e1cc908b0352d9cd221c9f75e591a4fe5f1f
Korobeynik <korobeynik77@gmail.com>
2020-07-22 15:06:49 +0300

    create react app

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container pt-3">
+      <div className="row">
+          <div className="col">Form</div>
+      </div>
+        <div className="row">
+            <div className="col">
+                POSTS
+            </div>
+            <div className="col">
+                Fetched posts
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
